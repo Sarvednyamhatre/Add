086ea4f80ede3c77f5e788370ddbbd169ee420e7
Sarvednya Mhatre <sarvednya2@gmail.com>
2024-04-29 21:35:51 +0530

    Initial commit of Addition Java program

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 5; // First number
+        int num2 = 3; // Second number
+        System.out.println("Sum: " + (num1 + num2));
+    }
+}
